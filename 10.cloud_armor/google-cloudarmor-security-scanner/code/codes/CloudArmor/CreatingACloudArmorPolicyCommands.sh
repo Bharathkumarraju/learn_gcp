f4c0eec3053b906d7e1f9008636f847da265c459
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2020-08-15 09:37:21 +0800

    google cloud armor

--- 10.cloud_armor/google-cloudarmor-security-scanner/code/codes/CloudArmor/CreatingACloudArmorPolicyCommands.sh
+++ 10.cloud_armor/google-cloudarmor-security-scanner/code/codes/CloudArmor/CreatingACloudArmorPolicyCommands.sh
@@ -26,6 +26,15 @@ gcloud beta compute security-policies rules delete 2147483647 \
 
 
 
+configure firewall:
+------------------->
+
+firewall1: allow-http-traffic:
+
+firewall2: allow-health-check
+
+use google managed ips:  130.211.0.0/22 and 35.191.0.0/16
+
 
 
 
